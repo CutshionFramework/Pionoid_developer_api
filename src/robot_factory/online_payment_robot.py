72c7389bd685e9f41acdbb87246da4764bfdc559
devRyan9025 <alex7006@naver.com>
2024-09-05 09:49:54 +0900

    Test online payment feature for UR and added example

--- src/robot_factory/online_payment_robot.py
+++ src/robot_factory/online_payment_robot.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from robot.custom_robots.jaka_robot import JakaRobot
+from robot.custom_robots.UR_robot import URRobot
 from integrations.stripe_online_payment import StripePayment
 
 
@@ -30,4 +31,34 @@ if __name__ == "__main__":
         source="tok_visa",  # Stripe's test card token for SUC
         #source="tok_chargeDeclined",  # Stripe's test card token for a declined charge
         description="Test Charge"
+    )
+    
+    # ---------------------------- UR Example -------------------------------
+    
+    
+    def handle_payment_and_robot_movement(api_key, amount, currency, source, description):
+        stripe_payment = StripePayment(api_key)
+        payment_successful = stripe_payment.process_payment(amount, currency, source, description)
+        
+        if payment_successful:
+            print("Payment succeeded, moving the robot.")
+            robot = URRobot("192.168.19.128")
+            robot.login()
+            robot.power_on()
+            robot.enable_robot()
+            robot.joint_move(joint_pos=[1.5, 0, -1.5, 0, 0, 0], move_mode=0, is_block=False, speed=2)
+            robot.logout()
+        else:
+            print("Payment failed, robot will not move.")
+
+# Example usage
+if __name__ == "__main__":
+    handle_payment_and_robot_movement(
+        api_key="sk_test_4eC39HqLyjWDarjtT1zdp7dc",  # Stripe's test API key
+        amount=8000,  # Amount in cents
+        currency="usd",
+        source="tok_visa",  # Stripe's test card token for SUC
+        #source="tok_chargeDeclined",  # Stripe's test card token for a declined charge
+        description="Test Charge"
+    
     )
